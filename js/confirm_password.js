ae64f1fdf48c48c6fa229d7f79c5bef06e6f830c
frexcuadillera <fcuadillera@gmail.com>
2018-07-01 02:13:49 +0800

    Create confirm_password.js

---
+++ js/confirm_password.js
@@ -0,0 +1,13 @@
+var password = document.getElementById("passwd_create")
+  , confirm_password = document.getElementById("passwd2_create");
+
+function validatePassword(){
+  if(password.value != confirm_password.value) {
+    confirm_password.setCustomValidity("Passwords Don't Match");
+  } else {
+    confirm_password.setCustomValidity('');
+  }
+}
+
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
